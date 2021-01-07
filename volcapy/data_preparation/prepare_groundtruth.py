b886e9f0bb32571604390d93aeea9aecec16acdc
Cedric Travelletti <cedrictravelletti@gmail.com>
2021-01-07 13:48:27 +0100

    Clean version of prepare groundtruth.

--- volcapy/data_preparation/prepare_groundtruth.py
+++ volcapy/data_preparation/prepare_groundtruth.py
@@ -1,5 +1,7 @@
 """ Prepare ground truth for article experiments by samling from Niklas
 posterior. We here use a small discretization.
+
+AWS Version: we just update prior samples generated offline.
 """
 from volcapy.inverse.inverse_problem import InverseProblem
 from volcapy.inverse.inverse_gaussian_process import InverseGaussianProcess
@@ -26,24 +28,41 @@ gpu = torch.device('cuda:0')
 cpu = torch.device('cpu')
 
 
-def prepare_groundtruth(input_path):
+def prepare_groundtruth(
+        data_path, prior_sample_path,
+        post_sample_path, post_data_sample_path):
+    """ Given a realization from the prior, compute the corresponding
+    conditional realization by updating.
+
+    Parameters
+    ----------
+    data_path: string
+        Path to the static data defining the situation (grid, forward, ...).
+    prior_sample_path: string
+        Path ta a realization from the prior.
+    post_sample_path: float
+        Where to save the computed posterior realization.
+    post_data_sample_path: string
+        Where to save the computed posterior realization of the data.
+
+    """
     # Load
     F = torch.from_numpy(
-            np.load(os.path.join(input_path, "F_niklas.npy"))).float().detach()
+            np.load(os.path.join(data_path, "F_niklas.npy"))).float().detach()
     F_full = torch.from_numpy(
-            np.load(os.path.join(input_path, "F_full_surface.npy"))).float().detach()
+            np.load(os.path.join(data_path, "F_full_surface.npy"))).float().detach()
 
-    grid = Grid.load(os.path.join(input_path,
+    grid = Grid.load(os.path.join(data_path,
                     "grid.pickle"))
     volcano_coords = torch.from_numpy(
             grid.cells).float().detach()
 
     data_coords = torch.from_numpy(
-            np.load(os.path.join(input_path, "niklas_data_coords.npy"))).float()
+            np.load(os.path.join(data_path, "niklas_data_coords.npy"))).float()
     data_coords_full = torch.from_numpy(
-            np.load(os.path.join(input_path,"surface_data_coords.npy"))).float()
+            np.load(os.path.join(data_path,"surface_data_coords.npy"))).float()
     data_values = torch.from_numpy(
-            np.load(os.path.join(input_path,"niklas_data_obs.npy"))).float()
+            np.load(os.path.join(data_path,"niklas_data_obs.npy"))).float()
 
     print("Size of inversion grid: {} cells.".format(volcano_coords.shape[0]))
     print("Number of datapoints: {}.".format(data_coords.shape[0]))
@@ -51,6 +70,9 @@ def prepare_groundtruth(input_path):
     cov_size = (volcano_coords.shape[0])**2 * 4 / 1e9
     print("Size of Covariance matrix: {} GB.".format(cov_size))
     print("Size of Pushforward matrix: {} GB.".format(size))
+
+    prior_sample = torch.from_numpy(
+            np.load(prior_sample_path)).float()
 
     # HYPERPARAMETERS, small volcano.
     data_std = 0.1
@@ -62,38 +84,37 @@ def prepare_groundtruth(input_path):
             volcano_coords, kernel,
             logger=logger)
 
-    for i in range(n_realizations):
-        start = timer()
-    
-        post_sample = myGP.sample_posterior(F, data_values, data_std)
-    
-        end = timer()
-        print("Posterior sampling run in {}s.".format(end - start))
-    
-        """
-        irregular_array_to_point_cloud(volcano_coords.numpy(),
-                post_sample.detach().cpu().numpy(),
-                os.path.join(input_path, "post_sample.vtk"), fill_nan_val=-20000.0)
-    
-        prior_sample = myGP.sample_prior()
-        irregular_array_to_point_cloud(volcano_coords.numpy(),
-                prior_sample.detach().cpu().numpy(),
-                os.path.join(input_path, "prior_sample.vtk"), fill_nan_val=-20000.0)
-    
-        """
-        np.save(os.path.join(input_path, "post_sample_{}.npy".format(i)),
-                post_sample.detach().cpu().numpy())
-        """
-        np.save(os.path.join(input_path, "prior_sample.npy"),
-                prior_sample.detach().cpu().numpy())
-        """
-        post_data_sample = F_full @ post_sample
-        np.save(os.path.join(input_path, "post_data_sample_{}.npy".format(i)),
-                post_data_sample.detach().cpu().numpy())
+    start = timer()
+
+    post_sample = myGP.update_sample(prior_sample, F, data_values, data_std)
+
+    end = timer()
+    print("Sample updating run in {}s.".format(end - start))
+
+    np.save(post_sample_path,
+            post_sample.detach().cpu().numpy())
+
+    post_data_sample = F_full @ post_sample
+    np.save(post_data_sample_path,
+            post_data_sample.detach().cpu().numpy())
 
 
 if __name__ == "__main__":
-    print("Usage: prepare_groundtruth.py input_path n_realizations")
-    input_path = sys.argv[1]
-    n_realizations = int(sys.argv[2])
-    prepare_groundtruth(input_path)
+    # data_path = "/home/ubuntu/Dev/VolcapyProd/data/InversionDatas/stromboli_173018_cells/"
+    data_path = "/home/cedric/PHD/Dev/VolcapySIAM/data/InversionDatas/stromboli_23823_cells/"
+    # prior_samples_folder = "/home/ubuntu/Dev/VolcapyProd/data/InversionDatas/stromboli_173018_cells/prior_samples/"
+    prior_samples_folder = "/home/cedric/PHD/Dev/VolcapySIAM/data/InversionDatas/stromboli_23823_cells/prior_samples/"
+    post_samples_folder = "/home/cedric/PHD/Dev/VolcapySIAM/data/InversionDatas/stromboli_23823_cells/post_samples/"
+    post_data_samples_folder = "/home/cedric/PHD/Dev/VolcapySIAM/data/InversionDatas/stromboli_23823_cells/post_data_samples/"
+
+    for i in range(1, 4):
+        print("Generating sample nr. {}.".format(i))
+        prior_sample_path = os.path.join(
+                prior_samples_folder, "prior_sample_{}.npy".format(i))
+        post_sample_path = os.path.join(
+                post_samples_folder, "post_sample_{}.npy".format(i))
+        post_data_sample_path = os.path.join(
+                post_data_samples_folder, "post_data_sample_{}.npy".format(i))
+
+        prepare_groundtruth(data_path, prior_sample_path,
+                post_sample_path, post_data_sample_path)
