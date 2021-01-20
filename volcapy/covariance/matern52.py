Corrected variance extraction and implemeneted covariance extraction (in InverseGaussianProcess).

volcapy/covariance/matern52.py
@@ -64,7 +64,7 @@ def compute_cov_pushforward(lambda0, F, cells_coords, device=None, n_chunks=200,
         torch.cuda.empty_cache()
 
     inv_lambda2 = - np.sqrt(5) / lambda0
-    n_dims = 3
+    n_dims = cells_coords.shape[1]
     n_model = F.shape[1]
 
     # Array to hold the results. We will compute line by line and concatenate.
@@ -170,3 +170,94 @@ def compute_cov(lambda0, cells_coords, i, j):
             , 2).sum())
 
     return (1 - inv_lambda2 * d + (1/3) * inv_lambda2**2 * d**2) * torch.exp(inv_lambda2 * d)
+
+def compute_covariance(lambda0, cells_coords, device=None, n_chunks=200,
+        n_flush=50):
+    """ Compute the covariance pushforward.
+
+    The covariance pushforward is just KF^T, where K is the model
+    covariance matrix.
+
+    Note that the sigam0^2 is not included, and one has to manually add it when
+    using the covariance pushforward computed here.
+
+    Parameters
+    ----------
+    lambda0: float
+        Lenght-scale parameter
+    F: tensor
+        Forward operator matrix
+    cells_coords: tensor
+        n_cells * n_dims: cells coordinates
+    device: toch.Device
+        Device to perform the computation on, CPU or GPU.
+        Defaults to None, in which case first tries gpu and falls back to cpu
+        if unavailable.
+    n_chunks: int
+        Number of chunks to split the matrix into.
+        Default is 200. Increase if get OOM errors.
+    n_flush: int
+        Synchronize threads and flush GPU cache every *n_flush* iterations.
+        This is necessary to avoid OOM errors.
+        Default is 50.
+
+    Returns
+    -------
+    Tensor
+        n_model * n_data covariance pushforward K F^t.
+    """
+    start = timer()
+
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Transfer everything to device.
+    lambda0 = torch.tensor(lambda0, requires_grad=False).to(device)
+    cells_coords = cells_coords.detach().to(device)
+
+    # Flush to make sure everything clean.
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()
+
+    inv_lambda2 = - np.sqrt(5) / lambda0
+    n_dims = cells_coords.shape[1]
+    n_model = cells_coords.shape[0]
+
+    # Array to hold the results. We will compute line by line and concatenate.
+    tot = torch.Tensor().to(device)
+
+    # Compute K chunk by chunk.
+    # That is, of all the cell couples, we compute the distance between some
+    # cells (here x) and ALL other cells. Then repeat for other chunk and
+    # concatenate.
+    for i, x in enumerate(torch.chunk(cells_coords, chunks=n_chunks, dim=0)):
+        # Empty cache every so often. Otherwise we get out of memory errors.
+        if i % n_flush == 0 and torch.cuda.is_available():
+            torch.cuda.synchronize()
+            torch.cuda.empty_cache()
+
+        # Euclidean distance.
+        d = torch.sqrt(torch.pow(
+            x.unsqueeze(1).expand(x.shape[0], n_model, n_dims)
+            - cells_coords.unsqueeze(0).expand(x.shape[0], n_model, n_dims)
+            , 2).sum(2))
+        tot = torch.cat((
+                tot,
+                    torch.mul(
+                        (
+                            torch.ones(d.shape, device=device)
+                            - inv_lambda2 * d
+                            + (1/3) * inv_lambda2**2 * d**2),
+                        torch.exp(inv_lambda2 * d))
+                    ))
+
+    # Wait for all threads to complete.
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()
+
+    end = timer()
+    print(end - start)
+
+    return tot.detach().cpu()

volcapy/inverse/inverse_gaussian_process.py
@@ -668,14 +668,35 @@ class InverseGaussianProcess(torch.nn.Module):
             Variance at each point.
 
         """
-        prior_variances = self.sigma0**2 * self.kernel.compute_diagonal(
+        prior_variances = self.kernel.compute_diagonal(
                 self.lambda0, self.cells_coords, DEVICE,
                 n_chunks=self.n_chunks, n_flush=50)
 
-        prior_variances -= torch.einsum("ij,jk,ik->i",
-                self.pushfwd, self.inversion_operator, self.pushfwd)
+        prior_variances -= self.sigma0**2 * torch.einsum("ij,jk,ik->i",
+                self.pushfwd.double(), self.inversion_operator,
+                self.pushfwd.double())
 
-        return prior_variances
+        return self.sigma0**2 * prior_variances
+
+    def posterior_covariance(self):
+        """ Computes posterior covariance matrix.
+        WARNING: needs pushforward and inversion operator to be only computed,
+        hence, only call after having conditioned the model.
+
+        Returns
+        -------
+        cov_mat: (self.cells_coords.n_cells, self.cells_coords.n_cells) Tensor
+            Posterior covariance matrix.
+
+        """
+        prior_covariance = self.kernel.compute_covariance(
+                self.lambda0, self.cells_coords, DEVICE,
+                n_chunks=self.n_chunks, n_flush=50)
+
+        prior_covariance -= self.sigma0**2 * (self.pushfwd.double() @ self.inversion_operator 
+                @ self.pushfwd.double().t())
+
+        return self.sigma0**2 * prior_covariance
 
     def sample_posterior(self, G, y, data_std):
         """ Sample from the posterior using residual kriging.