73bfac237d9b7ad1252cfdc582a756f5bb09d886
Cedric Travelletti <cedrictravelletti@gmail.com>
2021-02-19 06:17:41 +0100

    One dimensional indexing for the 2D Fourier coefficients.

--- volcapy/inverse/toy_fourier_2d.py
+++ volcapy/inverse/toy_fourier_2d.py
@@ -20,10 +20,11 @@ class ToyFourier2d():
     G_re: array
 
     """
-    def __init__(self, grid, G_re, G_im):
+    def __init__(self, grid, G_re, G_im, n_cells_1d):
         self.grid = grid
         self.G_re = G_re
         self.G_im = G_im
+        self.n_cells_1d = n_cells_1d
 
     @classmethod
     def build_problem(cls, n_cells_1d):
@@ -45,4 +46,11 @@ class ToyFourier2d():
         N = n_cells_1d
         G_re, G_im = compute_forward(grid.cells, M, N, n_procs=4)
         
-        return ToyInverseProblem2d(grid, G_re, G_im)
+        return cls(grid, G_re, G_im, n_cells_1d)
+
+    def index_to_1d(self, m, n):
+        """ Converts a 2D index (in Fourier space) to a 1D index, which allows
+        to retrieve the location inside the forward operator.
+
+        """
+        return np.ravel_multi_index((m, n), (self.n_cells_1d, self.n_cells_1d))
