5e7718b7d572c42dc401614e7e781aabc0e0438a
Cedric Travelletti <cedrictravelletti@gmail.com>
2021-06-24 22:51:06 +0200

    Prior sampling with final parameters.

--- volcapy/data_preparation/sample_prior.py
+++ volcapy/data_preparation/sample_prior.py
@@ -13,8 +13,7 @@ import os
 import torch
 
 
-def sample_prior(input_path, n_realizations):
-    output_path = os.path.join(input_path, "prior_samples_v2/")
+def sample_prior(input_path, output_path, n_realizations):
     os.makedirs(output_path, exist_ok=True)
 
     # Load
@@ -25,16 +24,25 @@ def sample_prior(input_path, n_realizations):
 
     # HYPERPARAMETERS.
     data_std = 0.1
-    sigma0 = 359.49
-    m0 = -114.40
-    lambda0 = 338.46
 
-    myGP = InverseGaussianProcess(m0, sigma0, lambda0,
+    sigma0_exp = 308.89
+    m0_exp = 535.39
+    lambda0_exp = 1925.0
+
+    sigma0_matern32 = 527.84
+    m0_matern32 = 549.15
+    lambda0_matern32 = 891.66
+
+    sigma0_matern52 = 349.47
+    m0_matern52 = 582.43
+    lambda0_matern52 = 436.206
+
+    myGP = InverseGaussianProcess(m0_matern52, sigma0_matern52,
+            lambda0_matern52,
             volcano_coords, kernel,
             n_chunks=70, n_flush=50)
 
-    # TODO: Change. Here we had already produced 3 samples.
-    for i in range(1, n_realizations):
+    for i in range(n_realizations):
         start = timer()
         prior_sample = myGP.sample_prior()
         end = timer()
@@ -45,7 +53,8 @@ def sample_prior(input_path, n_realizations):
 
 
 if __name__ == "__main__":
-    print("Usage: sample_prior.py input_path n_realizations")
+    print("Usage: sample_prior.py input_path output_path n_realizations")
     input_path = sys.argv[1]
-    n_realizations = int(sys.argv[2])
-    sample_prior(input_path, n_realizations)
+    output_path = sys.argv[2]
+    n_realizations = int(sys.argv[3])
+    sample_prior(input_path, output_path, n_realizations)
