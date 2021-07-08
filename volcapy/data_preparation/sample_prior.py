8f488349b0975b44230327461d552c783f133731
Cedric Travelletti <cedrictravelletti@gmail.com>
2021-07-08 15:43:03 +0200

    New hyperparameters for prior sampling.

--- volcapy/data_preparation/sample_prior.py
+++ volcapy/data_preparation/sample_prior.py
@@ -2,7 +2,7 @@
 
 """
 from volcapy.inverse.inverse_gaussian_process import InverseGaussianProcess
-import volcapy.covariance.matern52 as kernel
+import volcapy.covariance.matern32 as kernel
 from volcapy.grid.grid_from_dsm import Grid
 import sys
 
@@ -25,24 +25,24 @@ def sample_prior(input_path, output_path, n_realizations):
     # HYPERPARAMETERS.
     data_std = 0.1
 
-    sigma0_exp = 308.89
+    sigma0_exp = 308.89 # WARNING: Old values.
     m0_exp = 535.39
     lambda0_exp = 1925.0
 
-    sigma0_matern32 = 527.84
-    m0_matern32 = 549.15
-    lambda0_matern32 = 891.66
+    sigma0_matern32 = 284.66
+    m0_matern32 = 2139.1
+    lambda0_matern32 = 651.58
 
-    sigma0_matern52 = 349.47
-    m0_matern52 = 582.43
-    lambda0_matern52 = 436.206
+    sigma0_matern52 = 258.40
+    m0_matern52 = 2120.93
+    lambda0_matern52 = 441.05
 
-    myGP = InverseGaussianProcess(m0_matern52, sigma0_matern52,
-            lambda0_matern52,
+    myGP = InverseGaussianProcess(m0_matern32, sigma0_matern32,
+            lambda0_matern32,
             volcano_coords, kernel,
             n_chunks=70, n_flush=50)
 
-    for i in range(n_realizations):
+    for i in range(500, 500 + n_realizations):
         start = timer()
         prior_sample = myGP.sample_prior()
         end = timer()
