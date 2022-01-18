67fd70e30def40aaa911490d76b2f8a5ef7a86af
Cedric Travelletti <cedrictravelletti@gmail.com>
2022-01-18 15:44:46 +0100

    First draft for conservative SUR strategies.

---
+++ volcapy/strategy/conservative_strategies.py
@@ -0,0 +1,61 @@
+""" SUR strategies based on conservative estimates. 
+This module implements the criterions defined in D. Azzimonti's 
+thesis, chapter 5.2.
+
+"""
+import torch
+import numpy as np
+from volcapy.strategy.strategyABC import StrategyABC
+
+
+class ConservativeStrategy(StrategyABC):
+    def compute_helper_quantities(self, G, data_std):
+        """ Compute helper quantities a_n, b_n defined 
+        in eq. 5.9.
+        WARNING: Only works for excursion above threshold.
+
+        Parameters
+        ----------
+        G: Tensor (n_obs, n_cells)
+            Observation operator.
+        data_std: float
+            Observational noise standard deviation.
+
+        Returns
+        -------
+        a: Tensor (n_cells)
+        b: Tensor (n_cells, n_obs)
+
+        """
+        mean = self.gp.mean_vec
+        fantasy_variance, G_dash = self.gp.covariance.compute_fantasy_variance(G, data_std)
+        fantasy_std = torch.sqrt(fantasy_variance).reshape(
+                fantasy_variance.shape[0], 1)
+
+        a = torch.div(
+                mean - self.lower * torch.ones(mean.shape),
+                fantasy_std)
+        K_q = G.double() @ G_dash.double()
+        b = torch.div(G_dash.double() @ torch.inverse(K_q),
+                fantasy_std.repeat(1, K_q.shape[0]).double()).float()
+        return (a, b)
+
+    def get_next_ind(self):
+        # Evaluate criterion on neighbors.
+        neighbors_inds = self.get_neighbors(self.current_ind)
+        neighbors_ivrs = []
+
+        print("Evaluating criterion among {} candidates.".format(neighbors_inds.shape[0]))
+
+        # Compute criterion for each candidate.
+        for ind in neighbors_inds:
+            # Observation operator for candidate location.
+            candidate_G = self.G[ind,:].reshape(1, -1)
+            ivr = self.gp.IVR(candidate_G, self.data_std, 
+                                weights=self.current_coverage)
+            neighbors_ivrs.append(ivr)
+
+        # Find best candidate.
+        tmp_ind = np.argmax(neighbors_ivrs)
+        best_ind = neighbors_inds[tmp_ind]
+        return best_ind
