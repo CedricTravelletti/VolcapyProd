0220519af87a5dab186f3fa053bfa0ccdda6d7e5
Cedric Travelletti <cedrictravelletti@gmail.com>
2022-04-20 15:00:26 +0200

    Final plotting script for Fourier in the paper.

--- reporting/Toy2dProblems/Fourier/plot_fourier_paper_FINAL.py
+++ reporting/Toy2dProblems/Fourier/plot_fourier_paper_FINAL.py
@@ -14,8 +14,18 @@ import torch
 import scipy.interpolate
 import matplotlib.pyplot as plt
 
+plt.rcParams["font.family"] = "Times New Roman"
+plot_params = {
+        'font.size': 25, 'font.style': 'oblique',
+        'axes.labelsize': 'small',
+        'axes.titlesize':'small',
+        'legend.fontsize': 'small',
+        'legend.title_fontsize': 'x-small'
+        }
+plt.rcParams.update(plot_params)
 
-results_folder = "/home/cedric/PHD/Dev/VolcapySIAM/reporting/Toy2dProblems/Fourier/results_fill_big"
+
+results_folder = "/home/cedric/PHD/Dev/VolcapySIAM/reporting/Toy2dProblems/Fourier/results_fill_big_05"
 
 
 def plot_values(grid, vals_1, vals_2, pts, title,
@@ -90,12 +100,13 @@ def plot_values_separate(grid, vals_1, vals_2, pts, title,
     fig = plt.figure()
     plt.imshow(gridded_data_1.T, extent=(min_x,max_x,min_y,max_y),
             origin='lower', cmap=cmap, vmin=vmin, vmax=vmax)
+    plt.colorbar()
     plt.scatter(pts[:, 0], pts[:, 1])
 
     plt.xlim([min_x, max_x])
     plt.ylim([min_y, max_y])
-    plt.colorbar()
 
+    plt.axis('off')
     plt.savefig(outfile1, bbox_inches='tight', pad_inches=0, dpi=400)
     plt.close()
 
@@ -107,16 +118,51 @@ def plot_values_separate(grid, vals_1, vals_2, pts, title,
     plt.xlim([min_x, max_x])
     plt.ylim([min_y, max_y])
 
+    plt.axis('off')
     plt.savefig(outfile2, bbox_inches='tight', pad_inches=0, dpi=400)
     plt.close()
 
+
+def plot_ground_truth(grid, ground_truth, cmap, vmin, vmax, outfile):
+    # Generate regular gridding over which to plot.
+    min_x = np.min(grid.cells[:, 0])
+    max_x = np.max(grid.cells[:, 0])
+    min_y = np.min(grid.cells[:, 1])
+    max_y = np.max(grid.cells[:, 1])
+
+    grid_x, grid_y = np.mgrid[min_x:max_x:grid.n_cells_1d*(1j),
+            min_y:max_y:grid.n_cells_1d*(1j)]
+
+    gridded_data = scipy.interpolate.griddata(
+            grid.cells, ground_truth.reshape(-1), (grid_x, grid_y), method='cubic')
+
+    fig = plt.figure()
+    plt.imshow(gridded_data.T, extent=(min_x,max_x,min_y,max_y),
+            origin='lower', cmap=cmap, vmin=vmin, vmax=vmax)
+    cbar = plt.colorbar()
+    cbar.set_ticks([-5, -2.5, 0, 2.5, 5])
+    cbar.set_ticklabels([-5, -2.5, 0, 2.5, 5])
+
+    plt.xlim([min_x, max_x])
+    plt.ylim([min_y, max_y])
+
+    plt.axis('off')
+    plt.savefig(outfile, bbox_inches='tight', pad_inches=0, dpi=400)
+    plt.close()
+
+
 n_cells_1d = 400
 
-my_problem = ToyFourier2d.build_problem(n_cells_1d, forward_cutoff=1)
+my_problem = ToyFourier2d.build_problem(n_cells_1d, forward_cutoff=2)
 
 ground_truth = np.load(os.path.join(results_folder, "ground_truth.npy")).reshape(-1)
-my_problem.grid.plot_values(ground_truth, cmap='jet', vmin=-6, vmax=6)
-my_problem.grid.plot_values(ground_truth, cmap='jet', vmin=-6, vmax=6,
+my_problem.grid.plot_values(ground_truth, cmap='RdBu', vmin=-5, vmax=5)
+
+"""
+my_problem.grid.plot_values(ground_truth, cmap='RdBu', vmin=-6, vmax=6,
+        outfile=os.path.join(results_folder, "ground_truth.png"))
+"""
+plot_ground_truth(my_problem.grid, ground_truth, cmap='RdBu', vmin=-5, vmax=5,
         outfile=os.path.join(results_folder, "ground_truth.png"))
 
 # ------------
@@ -143,11 +189,11 @@ for i in range(1, n_situations):
     stds_pts[:, i] = np.load(
             os.path.join(results_folder, "sigma_post_pt_{}.npy".format(i)))
 
-    my_problem.grid.plot_values(means_fourier[:, i], cmap='jet', vmin=-6,
-            vmax=6, outfile=os.path.join(results_folder,
+    plot_ground_truth(my_problem.grid, means_fourier[:, i], cmap='RdBu', vmin=-5,
+            vmax=5, outfile=os.path.join(results_folder,
             "post_m_fourier_{}.png".format(i)))
-    my_problem.grid.plot_values(means_pts[:, i], cmap='jet', vmin=-6,
-            vmax=6, outfile=os.path.join(results_folder,
+    plot_ground_truth(my_problem.grid, means_pts[:, i], cmap='RdBu', vmin=-5,
+            vmax=5, outfile=os.path.join(results_folder,
             "post_m_pts_{}.png".format(i)))
 
     pts = my_problem.grid.cells[subset_inds[:i], :]
@@ -156,6 +202,6 @@ for i in range(1, n_situations):
             np.sqrt(stds_fourier[:, i]),
             pts,
             str(i),
-            cmap='jet', vmin=0, vmax=2,
+            cmap='RdBu', vmin=0, vmax=2,
             outfile1=os.path.join(results_folder, "data_loc_{}.png".format(i)),
             outfile2=os.path.join(results_folder, "std_{}.png".format(i)))
