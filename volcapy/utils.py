27c6fbbb6dad0cc5a0dae11f91619664437d49a5
Cedric Travelletti <ct19x463@gnode03.ubelix.unibe.ch>
2022-06-02 10:30:57 +0200

    kMeans clustering for folds.

--- volcapy/utils.py
+++ volcapy/utils.py
@@ -2,6 +2,8 @@
 
 """
 import numpy as np
+import torch
+from sklearn.cluster import KMeans
 
 
 def _make_column_vector(y):
@@ -59,3 +61,54 @@ def r_sequence(n_samples, dim, n_cells_1d):
         z[i, :] = (0.5 + alpha*(i+1)) %1
         c[i, :] = (np.floor(n_cells_1d *z[i, :])).astype(int)
     return c
+
+def kMeans_clustering(k, data_coords):
+    """ Cluster data points into k clusters using kMeans.
+
+    Parameters
+    ----------
+    k: int
+        Number of clusters.
+    data_coords: array (n_data, n_dims)
+        Coordinates of the points to cluster.
+
+    Returns
+    -------
+    cluster_labels: array (n_data)
+        For each data point, the lable (int) of the cluster 
+        it belongs to. Labels start at 0.
+
+    """
+    if torch.is_tensor(data_coords): data_coords = data_coords.cpu().numpy()
+    kmeans = KMeans(init="random", n_clusters=k,
+            n_init=10, max_iter=300, random_state=42)
+    kmeans.fit(data_coords)
+    return kmeans.labels_
+
+def kMeans_folds(k, data_coords):
+    """ Computes folds using kMeans clustering. 
+    Returns a list of folds, each fold being described by a list of 
+    indices, corresponding to the points that belong to the cluster. 
+    One can then use each of theses lists as a list of points to be 
+    left out in cross-validation. 
+
+    Parameters
+    ----------
+    k: int
+        Number of clusters.
+    data_coords: array (n_data, n_dims)
+        Coordinates of the points to cluster.
+
+    Returns
+    -------
+    folds: List[array]
+        List of integer arrays, each describing the indices 
+        belonging to a given fold.
+
+    """
+    cluster_labels = kMeans_clustering(k, data_coords)
+    folds = []
+    for i in range(k):
+        fold_indices = np.argwhere(cluster_labels == i).flatten()
+        folds.append(fold_indices)
+    return folds
