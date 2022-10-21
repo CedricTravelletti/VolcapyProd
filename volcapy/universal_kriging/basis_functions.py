a3e2d7eb6226318621d398d073c31d90423958dd
Cedric Travelletti <cedrictravelletti@gmail.com>
2022-10-21 15:56:57 +0200

    Implemented cutoff for the fault lines and tanh sigmoid scaling.

--- volcapy/universal_kriging/basis_functions.py
+++ volcapy/universal_kriging/basis_functions.py
@@ -11,7 +11,8 @@ def cylindrical(coords, x0, y0):
     """
     return torch.sqrt((coords[:, 0] - x0)**2 + (coords[:, 1] - y0)**2)
 
-def planar(coords, x0, y0, z0, phi, theta):
+def planar(coords, x0, y0, z0, phi, theta,
+        cutoff_x0=None, cutoff_y0=None, cutoff_z0=None, cutoff_phi=None, cutoff_theta=None, fill_value=None):
     """ Planar basis function: distance from a plane throught (x0, y0, z0) with 
     normal vector given by theta and phi in spherical coordinates.
     
@@ -21,7 +22,46 @@ def planar(coords, x0, y0, z0, phi, theta):
         [torch.cos(phi) * torch.sin(theta)],
         [torch.sin(phi) * torch.sin(theta)],
         [torch.cos(theta)]])
-    return torch.abs(n[0] * (coords[:, 0] - x0) + n[1] * (coords[:, 1] - y0) + n[2] * (coords[:, 2] - z0))
+
+    # Compute distance to plane.
+    dists = torch.abs(n[0] * (coords[:, 0] - x0) + n[1] * (coords[:, 1] - y0) + n[2] * (coords[:, 2] - z0))
+
+    # TODO: The below is used to cut the fault line. 
+    # The cut is done according to the angle with a given cutoff vector. 
+    # As soon as the angle exceeds 90 degrees, values are set to zero.
+    if (cutoff_theta is not None and cutoff_phi is not None and fill_value is not None):
+        cutoff_phi, cutoff_theta = torch.tensor([np.radians(cutoff_phi)]), torch.tensor([np.radians(cutoff_theta)])
+        n_cutoff = torch.tensor([
+            [torch.cos(cutoff_phi) * torch.sin(cutoff_theta)],
+            [torch.sin(cutoff_phi) * torch.sin(cutoff_theta)],
+            [torch.cos(cutoff_theta)]])
+        # Compute sign of scalar product, to see on which side the points lie.
+        mask = (n_cutoff[0] * (coords[:, 0] - cutoff_x0) + n_cutoff[1] * (coords[:, 1] - cutoff_y0) + n_cutoff[2] * (coords[:, 2] - cutoff_z0) < 0)
+        dists[mask] = fill_value
+    return dists 
+
+def tanh_sigmoid(dists, saturation_length, inverted=False):
+    """ Rescales the computed distances (planar, cylindrical, ...) by a tanh sigmoid function.
+
+    Parameters
+    ----------
+    dists: torch.Tensor [n_pts]
+        Distances compute by another basis function (planar, cylindrical, ...).
+    saturation_length: float
+        Length at which to saturate the tanh (reaches 1).
+    inverted: bool, defaults to False
+        If True, then return a decreasing function, 1 - tanh(dists).
+
+    Returns
+    -------
+    vals: torch.Tensor [n_pts]
+
+    """
+    saturation_length = torch.tensor([saturation_length])
+    if inverted is False:
+         return torch.tanh(dists * (0.5 * np.pi / saturation_length))
+    elif inverted is True:
+         return 1 - torch.tanh(dists * (0.5 * np.pi / saturation_length))
 
 def _haar(x):
     if x < 0: return 0.0
