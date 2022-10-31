8e170cfde7857389fa1ddb0fac53cdb9024638bb
Cedric Travelletti <ct19x463@submit02.ubelix.unibe.ch>
2022-10-31 22:04:09 +0100

    Can specify minimal size in sampler.

--- volcapy/set_sampling.py
+++ volcapy/set_sampling.py
@@ -28,7 +28,7 @@ def uniform_bernoulli_sample(input_set, size):
     sampled_sets = [np.random.choice(input_set, size=size, replace=False) for size in sizes]
     return sampled_sets
 
-def uniform_size_uniform_sample(input_set, size):
+def uniform_size_uniform_sample(input_set, size, min_size=1, max_size=None):
     """ Sample set by first uniformly sampling a size in [1, input_set_size] 
     and then uniformly sampling a subset of that size.
 
@@ -42,7 +42,10 @@ def uniform_size_uniform_sample(input_set, size):
         List of sampled subarrays.
 
     """
+    if max_size is None:
+        max_size = len(input_set) + 1
+
     # Sample sizes uniformly.
-    sizes = np.random.randint(low=1, high=len(input_set) + 1, size=size)
+    sizes = np.random.randint(low=min_size, high=max_size, size=size)
     sampled_sets = [np.random.choice(input_set, size=size, replace=False) for size in sizes]
     return sampled_sets
