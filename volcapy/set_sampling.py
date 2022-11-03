e7dd709d04127b700e1e4e9c8ae2db2590909ca5
Cedric Travelletti <ct19x463@submit02.ubelix.unibe.ch>
2022-11-03 17:25:26 +0100

    Renamed variables in set_sampling.

--- volcapy/set_sampling.py
+++ volcapy/set_sampling.py
@@ -28,7 +28,7 @@ def uniform_bernoulli_sample(input_set, size):
     sampled_sets = [np.random.choice(input_set, size=size, replace=False) for size in sizes]
     return sampled_sets
 
-def uniform_size_uniform_sample(input_set, size, min_size=1, max_size=None, fixed_node=None):
+def uniform_size_uniform_sample(input_set, sample_size, min_size=1, max_size=None, fixed_node=None):
     """ Sample set by first uniformly sampling a size in [1, input_set_size] 
     and then uniformly sampling a subset of that size.
 
@@ -51,7 +51,7 @@ def uniform_size_uniform_sample(input_set, size, min_size=1, max_size=None, fixe
     else: high_bound = max_size + 1
 
     # Sample sizes uniformly.
-    sizes = np.random.randint(low=min_size, high=high_bound, size=size)
+    sizes = np.random.randint(low=min_size, high=high_bound, size=sample_size)
     # Sample random points and add the fixed node.
     sampled_sets = [np.random.choice(input_set, size=size, replace=False) for size in sizes]
 
